f6ce93776453e4e371f0d35f56d7c926375bde7a
danillistrovoy <listrovoy02@mail.ru>
2023-02-21 22:31:56 +1000

    Буду заливать задачи дальше, по мере решения, если что отмечаю в гугле прогресс

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+
+    private static class Task1 {
+        public static void main(String[] args) {
+            Scanner numbers = new Scanner(System.in);
+            System.out.println("Введите целое число №1");
+            int number1 = numbers.nextInt();
+            System.out.println("Введите целое число №2");
+            int number2 = numbers.nextInt();
+            int number3;
+            number3 = number1;
+            number1 = number2;
+            number2 = number3;
+            System.out.println("Теперь число 1 = " + number1+", а число 2 = " + number2 + ".");
+
+
+        }
+
+
+    }
+
+    private static class Task3 {
+        public static void main(String[] args) {
+            Scanner numbers = new Scanner(System.in);
+            System.out.println("Введите целое число №1");
+            int number1 = numbers.nextInt();
+            System.out.println("Введите целое число №2");
+            int number2 = numbers.nextInt();
+
+            int result = number1 < number2 ? number2 : number1;
+            System.out.println("Наибольшее из введённых чисел - " + result);
+
+
+        }
+
+    }
+}
