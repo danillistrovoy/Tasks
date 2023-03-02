08ad130ec49aa556013164458499a3faa939d0bc
danillistrovoy <listrovoy02@mail.ru>
2023-03-02 19:52:25 +1000

    Решил 1 и 2 задачи, есть вопросы по 14 задаче

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import com.sun.jdi.IntegerValue;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -148,7 +149,7 @@ public class Main {
             int[][] matrice = new int[n][n];
             for (int i = 0; i < matrice.length; i++) {
                 for (int j = 0; j < matrice.length; j++) {
-                    System.out.print((int)((Math.pow(2,j+1)) * Math.pow(2,i)) + " ");
+                    System.out.print((int) ((Math.pow(2, j + 1)) * Math.pow(2, i)) + " ");
                 }
                 System.out.println();
             }
@@ -214,16 +215,20 @@ public class Main {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
             int n = scanner.nextInt();
-            int[] chain = new int[n];
+            int[] chain = new int[n + 1];
             chain[0] = 0;
             chain[1] = 1;
-            System.out.print(chain[0] + " ");
-            System.out.print(chain[1] + " ");
-            for (int i = 2; i < n; ++i) {
-                chain[i] = chain[i - 1] + chain[i - 2];
-                System.out.print(chain[i] + " ");
-            }
+            if (n == 1) {
+                System.out.println(chain[1]);
+            } else if (n == 0) {
+                System.out.println(chain[0]);
+            } else {
+                for (int i = 2; i <= n; ++i) {
+                    chain[i] = chain[i - 1] + chain[i - 2];
 
+                }
+                System.out.println(chain[n]);
+            }
         }
 
 
@@ -247,6 +252,68 @@ public class Main {
         }
 
     }
+
+    private static int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    private static class Task16 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите номер числа последовательности Фибоначчи: ");
+            int numberfib = scanner.nextInt();
+            System.out.println(numberfib + " число последовательности Фибоначчи - " + fib(numberfib));
+        }
+    }
+
+    private static class Alg1 {
+        public static void main(String[] args) {
+            boolean sorted = false;
+
+            int[] array = new int[]{51, 20, 43, 24, 93, 37, 29, 45, 63, 78};
+            while (!sorted) {
+                sorted = true;
+
+                for (int i = 1; i < array.length; i++) {
+                    if (array[i] < array[i - 1]) {
+                        int temp = array[i];
+                        array[i] = array[i - 1];
+                        array[i - 1] = temp;
+                        sorted = false;
+                    }
+                }
+
+            }
+            for (int i = 0; i < array.length; i++) {
+                System.out.println(array[i] + " ");
+            }
+        }
+    }
+
+    private static class Alg3 {
+        public static void main(String[] args) {
+            int[] array = new int[] {16, 43, 20, 24, 93, 37, 29, 45, 63, 78};
+            for (int i = 0; i < array.length; i++) {
+                int a = array[i];
+                int j = i;
+                while (j > 0 && array[j - 1] >= a) {
+                    array[j] = array[j - 1];
+                    j--;
+                }
+                array[j] = array[i];
+            }
+            System.out.println(Arrays.toString(array));
+        }
+    }
+
+}
+
+
+
+
 
 
 
