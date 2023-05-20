ea3a4a2dfece62eee8239b40fa71f71315409cd5
danillistrovoy <listrovoy02@mail.ru>
2023-05-20 19:49:23 +1000

    Реализовал метод deleteNode

--- src/Structures/CustomBinarySearchTree/CustomBinarySearchTree.java
+++ src/Structures/CustomBinarySearchTree/CustomBinarySearchTree.java
@@ -4,23 +4,27 @@ public class CustomBinarySearchTree {
     Node root;
 
 
-    public void insertLeaf(Node node, int leafValue) {
-        if (root == null){
-            Node root = new Node(leafValue);
+    public void insertLeaf(int leafValue) {
+        if (root == null) {
+            this.root = new Node(leafValue);
             return;
         }
+        insert(root, leafValue);
+    }
+
+    private void insert(Node node, int leafValue) {
         if (node.value == leafValue) {
             return;
         }
         if (leafValue < node.value) {
             if (node.left != null) {
-                insertLeaf(node.left, leafValue);
+                insert(node.left, leafValue);
             } else {
                 node.left = new Node(leafValue);
             }
         } else {
             if (node.right != null) {
-                insertLeaf(node.right, leafValue);
+                insert(node.right, leafValue);
             } else {
                 node.right = new Node(leafValue);
             }
@@ -28,23 +32,65 @@ public class CustomBinarySearchTree {
     }
 
 
-    public void deleteNode(int value) {
+    public boolean deleteNode(int value) {
         if (root == null) {
-            throw new IllegalArgumentException();
+            return false;
         }
-        if (root.value == value){
-            Node newRoot = root.right;
-            root.right = root.right.right;
-            root = newRoot;
-            return;
+        if (root.value == value) {
+            if (root.left == null && root.right == null) {
+                root = null;
+            }
+            if (root.right != null && root.left != null) {
+                root = root.right;
+            }
+            if (root.left != null && root.right == null) {
+                root = root.left;
+            }
+            if (root.right.right == null && root.right.left != null) {
+                root = root.right.left;
+            }
+            if (root.left.right != null && root.right == null) {
+                root = root.left.right;
+            }
+            delete(root, value);
         }
-        if (value < root.value){
-            if (root.left.value==value){
+        return true;
+    }
 
+    private void delete(Node node, int value) {
+        if (value < node.value) {
+            if (value == node.left.value) {
+                if (node.left.right == null && node.left.left == null) {
+                    node.left = null;
+                }
+                if (node.left.right != null && node.left.right.left == null) {
+                    node.left = node.left.right;
+                }
+                if (node.left.right.left != null && node.left.left != null) {
+                    node.left = node.left.right.left;
+                } else {
+                    delete(node.left, value);
+                }
+            }
+        } else {
+            if (value == node.right.value) {
+                if (node.right.right == null && node.right.left == null) {
+                    node.right = null;
+                }
+                if (node.right.right != null && node.right.right.left == null) {
+                    node.right = node.right.right;
+                }
+                if (node.right.right.left != null && node.right.left != null) {
+                    node.right = node.right.right.left;
+                }
+            } else {
+                delete(node.right, value);
             }
         }
     }
-    public void printTree (){
+
+
+    public void printTree() {
 
     }
 
